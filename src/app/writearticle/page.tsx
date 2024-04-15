8104a3251a26cca4e43e8a65a816fda024ce1ae7
adrianbgo <adrianbgo@hotmail.com>
2024-04-15 08:40:37 -0400

    feat: added writearticle page

---
+++ src/app/writearticle/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+import { Article } from "../components/Article";
+import { Header } from "../components/Header";
+
+const WriteArticle = () => (
+  <>
+    <Header />
+    <Article />
+  </>
+);
+
+export default WriteArticle;
